3deb324e2418bd2e7a4ac144ad320331fd76874f
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2011-01-30 14:59:43 +0100

    display station details when touching an annotation

--- Files/MapVC.m
+++ Files/MapVC.m
@@ -11,6 +11,7 @@
 #import "VelibDataManager.h"
 #import "Station.h"
 #import "Region.h"
+#import "StationDetailVC.h"
 
 typedef enum {
 	MapModeNone = 0,
@@ -21,6 +22,8 @@ typedef enum {
 @interface MapVC() <MKMapViewDelegate>
 @property (nonatomic) MKCoordinateRegion referenceRegion;
 @property (nonatomic) MapMode mode;
+- (void) showDetails:(MKAnnotationView*)sender;
+- (void) zoomIn:(Region*)region;
 @end
 
 /****************************************************************************/
@@ -50,18 +53,67 @@ typedef enum {
 }
 
 /****************************************************************************/
-#pragma mark -
+#pragma mark MapView Delegate
 
 - (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated
 {
 	CLLocationDegrees modelSpan = self.referenceRegion.span.latitudeDelta;
 	if(self.mapView.region.span.latitudeDelta>modelSpan*2.0f)
 		self.mode = MapModeNone;
-	else if(self.mapView.region.span.latitudeDelta>modelSpan/16.0f)
+	else if(self.mapView.region.span.latitudeDelta>modelSpan/10.0f)
 		self.mode = MapModeRegions;
 	else
 		self.mode = MapModeStations;
 }
+
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation
+{
+	if([annotation isKindOfClass:[MKUserLocation class]])
+		return nil;
+	else if([annotation isKindOfClass:[Region class]])
+	{
+		NSString* identifier = NSStringFromClass([Region class]);
+		MKPinAnnotationView * pinView = (MKPinAnnotationView*)[self.mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
+		if(nil==pinView)
+		{
+			pinView = [[[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier] autorelease];
+			pinView.pinColor = MKPinAnnotationColorPurple;
+			pinView.canShowCallout = NO;
+		}
+		return pinView;
+	}
+	else if([annotation isKindOfClass:[Station class]])
+	{
+		NSString* identifier = NSStringFromClass([Station class]);
+		MKPinAnnotationView * pinView = (MKPinAnnotationView*)[self.mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
+		if(nil==pinView)
+		{
+			pinView = [[[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier] autorelease];
+			pinView.pinColor = MKPinAnnotationColorGreen;
+			pinView.canShowCallout = YES;
+			UIButton* rightButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+			[rightButton addTarget:self
+							action:@selector(showDetails:)
+				  forControlEvents:UIControlEventTouchUpInside];
+			pinView.rightCalloutAccessoryView = rightButton;
+		}
+		return pinView;
+	}
+	return nil;
+}
+
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view
+{
+	Region * region = (Region*)view.annotation;
+	if([region isKindOfClass:[Region class]])
+	{
+		[self zoomIn:region];
+	}
+}
+
+/****************************************************************************/
+#pragma mark Actions
 
 - (void) setMode:(MapMode)value
 {
@@ -76,6 +128,19 @@ typedef enum {
 		[request setEntity:[class entityInManagedObjectContext:BicycletteAppDelegate.dataManager.moc]];
 		[self.mapView addAnnotations:[BicycletteAppDelegate.dataManager.moc executeFetchRequest:request error:NULL]];
 	}
+}
+
+
+- (void) showDetails:(UIButton*)sender
+{
+	Station * station = (Station*)[self.mapView.selectedAnnotations objectAtIndex:0];
+	
+	[self.navigationController pushViewController:[StationDetailVC detailVCWithStation:station inArray:nil] animated:YES];
+}
+
+- (void) zoomIn:(Region*)region
+{
+	[self.mapView setRegion:[self.mapView regionThatFits:region.coordinateRegion] animated:YES];
 }
 
 @end
