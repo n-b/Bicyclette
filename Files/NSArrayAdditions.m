7e0602b2d410a699e5e5e67cc97d450832b2216f
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2011-09-21 01:11:53 +0200

    ARC conversion prechecks, performSelector -> objc_msgSend.

--- Files/NSArrayAdditions.m
+++ Files/NSArrayAdditions.m
@@ -5,6 +5,7 @@
 //
 
 #import "NSArrayAdditions.h"
+#import "objc/message.h"
 
 
 @implementation NSArray (Additions)
@@ -39,9 +40,13 @@
 	
 	for (id object in self)
 	{
-		// we need to only evaluate a BOOL, not the 4-byte result of performSelector
-		// (see the literature on performSelector for details)
-		BOOL res = (BOOL) (long) [object performSelector:aFilterSelector];
+		// we need to only evaluate a BOOL, not the 4-byte or 8-byte result of performSelector
+		// (see the literature on performSelector and ARC for details)
+        //
+        // * (long) casts the C-style pointer to an integer of the same byte count as a pointer.
+        // * (BOOL) finally makes sure we only use the lower byte
+		BOOL res = (BOOL) (long) objc_msgSend(object, aFilterSelector);
+        
 		if( res )
 			[objects addObject:object];
 	} 
