1ef5c1254726857db0af849229cd83d80f28d330
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2011-11-27 22:11:50 +0100

    Added LogicTests target, cleaned up.

---
+++ LogicTests/BicycletteLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  BicycletteLogicTests.h
+//  BicycletteLogicTests
+//
+//  Created by Nicolas Bouilleaud on 27/11/11.
+//  Copyright (c) 2011 Visuamobile. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BicycletteLogicTests : SenTestCase
+
+@end
