e99d78d0e59abe9921e3f3c02b6d97bce848cccf
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2011-11-28 08:39:42 +0100

    Tests for basic KVCMapping.

--- LogicTests/NSObject+KVCMapping_Tests.m
+++ LogicTests/NSObject+KVCMapping_Tests.m
@@ -11,11 +11,26 @@
 @end
 
 @implementation NSObject_KVCMapping_Tests
+{
+    NSDictionary * mapping;
+}
 
+- (void) setUp
+{
+    [super setUp];
+    
+    mapping = [NSDictionary dictionaryWithObject:@"actualName" forKey:@"usedName"];
+}
 
 // All code under test is in the iOS Application
 - (void)testAppDelegate
 {
+    NSMutableDictionary * test = [NSMutableDictionary dictionary];
+    [test setValue:@"testValue" forKey:@"usedName" withMappingDictionary:mapping];
+    STAssertNil([test valueForKey:@"usedName"], nil);
+    STAssertEqualObjects([test valueForKey:@"actualName"], @"testValue", nil);
+    NSManagedObject
 }
 
 @end
+
