a115d443de17a9422ae363411dcac69e88d36fff
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2011-11-29 23:30:19 +0100

    Added the ability to specify NSValueTransformers by name in KVCMapping dictionary. (and tests)

--- Files/NSObject+KVCMapping.m
+++ Files/NSObject+KVCMapping.m
@@ -11,11 +11,25 @@
 
 @implementation NSObject (NSObject_KVCMapping)
 
+- (void) setTransformedValue:(id)value forRealKey:(NSString*)realKey
+{
+    [self setValue:value forKey:realKey];
+}
+
 - (void) setValue:(id)value forKey:(NSString*)wantedKey withMappingDictionary:(NSDictionary*)kvcMappingDictionnary
 {
     NSString * realKey = [kvcMappingDictionnary objectForKey:wantedKey];
+    // find a value transformer
+    NSArray * realComponents = [realKey componentsSeparatedByString:@":"];
+    if(realComponents.count==2)
+    {
+        realKey = [realComponents objectAtIndex:1];
+        NSValueTransformer * transformer = [NSValueTransformer valueTransformerForName:[realComponents objectAtIndex:0]];
+        value = [transformer transformedValue:value];
+    }
+    
     if([realKey length])
-        [self setValue:value forKey:realKey];
+        [self setTransformedValue:value forRealKey:realKey];
 #if DEBUG
     else
         NSLog(@"ignored key : %@ for class %@",wantedKey, [self class]);
@@ -35,96 +49,92 @@
 
 @implementation NSManagedObject (NSObject_KVCMapping)
 
-- (void) setValue:(id)value forKey:(NSString*)wantedKey withMappingDictionary:(NSDictionary*)kvcMappingDictionnary
+- (void) setTransformedValue:(id)value forRealKey:(NSString*)realKey
 {
-    NSString * realKey = [kvcMappingDictionnary objectForKey:wantedKey];
-    
-    NSAttributeDescription * attributeDesc = [[[(NSManagedObject*)self entity] attributesByName] objectForKey:realKey];
-    if(attributeDesc)
+    NSAttributeDescription * attributeDesc = [[[self entity] attributesByName] objectForKey:realKey];
+    if(attributeDesc==nil)
     {
-        id correctValue = nil;
-        
-        NSAttributeType attributeType = attributeDesc.attributeType;
-        Class expectedClass = NSClassFromString(attributeDesc.attributeValueClassName);
-
-        if([value isKindOfClass:expectedClass])
-            correctValue = value;
-        else
-        {
-            switch (attributeType) 
-            {
-                    // Numbers
-                    /*
-                     Notes :
-                     * Core data has only signed integers,
-                     * There's no "shortValue" in NSString,
-                     * Using intValue always return a 32-bit integer (an int), while integerValue returns an NSInteger, which may be 64-bit.
-                     */
-                case NSBooleanAttributeType :
-                    if([value respondsToSelector:@selector(boolValue)])
-                        correctValue = [NSNumber numberWithBool:[value boolValue]];  
-                    break;
-                case NSInteger16AttributeType :
-                case NSInteger32AttributeType :
-                    if([value respondsToSelector:@selector(intValue)])
-                        correctValue = [NSNumber numberWithLong:[value intValue]];  
-                    break;
-                case NSInteger64AttributeType :
-                    if([value respondsToSelector:@selector(longLongValue)])
-                        correctValue = [NSNumber numberWithLongLong:[value longLongValue]];  
-                    break;
-                case NSDecimalAttributeType :
-                    if([value isKindOfClass:[NSString self]])
-                        correctValue = [NSDecimalNumber decimalNumberWithString:value];  
-                    break;
-                case NSDoubleAttributeType :
-                    if([value respondsToSelector:@selector(doubleValue)])
-                        correctValue = [NSNumber numberWithDouble:[value doubleValue]];  
-                    break;
-                case NSFloatAttributeType :
-                    if([value respondsToSelector:@selector(floatValue)])
-                        correctValue = [NSNumber numberWithFloat:[value floatValue]];  
-                    break;
-                    
-                    // NSStrings
-                case NSStringAttributeType : 
-                    if([value respondsToSelector:@selector(stringValue)])
-                        correctValue = [value stringValue];
-                    break;
-                    
-                    // Date and Data
-                    // There is a need for specific date format parsing here.
-                case NSDateAttributeType :
-                case NSBinaryDataAttributeType:
-                    break;
-                    
-                    // Default behaviour for these (probably gonna crash later anyway)
-                case NSTransformableAttributeType:
-                case NSObjectIDAttributeType:
-                default :
-                    correctValue = value;
-                    break;
-            }
-#if DEBUG && DEBUG_KV_MAPPING
-            if(correctValue)
-                NSLog(@"fixed %@(%@) to %@(%@) for key %@(%@) of class %@",
-                      value, [value class], correctValue, [correctValue class], realKey, wantedKey, [self class]);
-            else
-                NSLog(@"invalid value : %@(%@), expected %@ for key %@(%@) of class %@",
-                      value, [value class], expectedClass, realKey, wantedKey, [self class]);
-#endif
-        }
-
-        NSAssert([[correctValue class] isSubclassOfClass:expectedClass],
-                 @"The result value %@(%@) is incorrect (expected %@) for key %@(%@) of class %@",
-                 correctValue, [correctValue class], expectedClass, realKey, wantedKey, [self class]);
-        if(correctValue)
-            [self setValue:correctValue forKey:realKey];
+        [super setTransformedValue:value forRealKey:realKey];
+        return;
     }
-#if DEBUG
+    
+    NSAttributeType attributeType = attributeDesc.attributeType;
+    Class expectedClass = NSClassFromString(attributeDesc.attributeValueClassName);
+    if([value isKindOfClass:expectedClass])
+    {
+        [self setValue:value forKey:realKey];
+        return;
+    }
+
+    id coercedValue = nil;
+    switch (attributeType) 
+    {
+            // Numbers
+            /*
+             Notes :
+             * Core data has only signed integers,
+             * There's no "shortValue" in NSString,
+             * Using intValue always return a 32-bit integer (an int), while integerValue returns an NSInteger, which may be 64-bit.
+             */
+        case NSBooleanAttributeType :
+            if([value respondsToSelector:@selector(boolValue)])
+                coercedValue = [NSNumber numberWithBool:[value boolValue]];  
+            break;
+        case NSInteger16AttributeType :
+        case NSInteger32AttributeType :
+            if([value respondsToSelector:@selector(intValue)])
+                coercedValue = [NSNumber numberWithLong:[value intValue]];  
+            break;
+        case NSInteger64AttributeType :
+            if([value respondsToSelector:@selector(longLongValue)])
+                coercedValue = [NSNumber numberWithLongLong:[value longLongValue]];  
+            break;
+        case NSDecimalAttributeType :
+            if([value isKindOfClass:[NSString self]])
+                coercedValue = [NSDecimalNumber decimalNumberWithString:value];  
+            break;
+        case NSDoubleAttributeType :
+            if([value respondsToSelector:@selector(doubleValue)])
+                coercedValue = [NSNumber numberWithDouble:[value doubleValue]];  
+            break;
+        case NSFloatAttributeType :
+            if([value respondsToSelector:@selector(floatValue)])
+                coercedValue = [NSNumber numberWithFloat:[value floatValue]];  
+            break;
+            
+            // NSStrings
+        case NSStringAttributeType : 
+            if([value respondsToSelector:@selector(stringValue)])
+                coercedValue = [value stringValue];
+            break;
+            
+            // Date, Data, Transformable : 
+            // We can't coerce automatically. 
+        case NSDateAttributeType :
+        case NSBinaryDataAttributeType:
+            break;
+            
+            // Default behaviour for these (probably gonna crash later anyway)
+        case NSObjectIDAttributeType:
+        case NSTransformableAttributeType:
+            break;
+        default :
+            coercedValue = value;
+            break;
+    }
+#if DEBUG && DEBUG_KV_MAPPING
+    if(correctValue)
+        NSLog(@"fixed %@(%@) to %@(%@) for key %@ of class %@",
+              value, [value class], correctValue, [correctValue class], realKey, [self class]);
     else
-        NSLog(@"ignored key : %@ for class %@",wantedKey, [self class]);
+        NSLog(@"invalid value : %@(%@), expected %@ for key %@ of class %@",
+              value, [value class], expectedClass, realKey, [self class]);
 #endif
+    NSAssert([[coercedValue class] isSubclassOfClass:expectedClass],
+             @"The result value %@(%@) is incorrect (expected %@) for key %@ of class %@",
+             coercedValue, [coercedValue class], expectedClass, realKey, [self class]);
+    
+    [self setValue:coercedValue forKey:realKey];
 }
 
 @end
--- LogicTests/NSObject+KVCMapping_Tests.m
+++ LogicTests/NSObject+KVCMapping_Tests.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2011 Nicolas Bouilleaud. All rights reserved.
 //
 #import "NSObject+KVCMapping.h"
+#import "FormattedStringToDateValueTransformer.h"
 
 @interface NSObject_KVCMapping_Tests : SenTestCase
 @end
@@ -18,12 +19,21 @@
 - (void) setUp
 {
     [super setUp];
+    mapping = [NSDictionary dictionaryWithObjectsAndKeys:
+               @"actualName", @"usedName",
+               @"ISO8601StringToDate:actualDate", @"usedDate",
+               nil];
     
-    mapping = [NSDictionary dictionaryWithObject:@"actualName" forKey:@"usedName"];
+    // Setup formatter and transformer
+    NSDateFormatter * dateFormatter = [NSDateFormatter new];
+	dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+    dateFormatter.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
+	[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+    NSValueTransformer * valueTransformer = [[FormattedStringToDateValueTransformer alloc] initWithDateFormatter:dateFormatter];
+    [NSValueTransformer setValueTransformer:valueTransformer forName:@"ISO8601StringToDate"];
 }
 
-// All code under test is in the iOS Application
-- (void)testAppDelegate
+- (void) testBasicMapping
 {
     NSMutableDictionary * test = [NSMutableDictionary dictionary];
     [test setValue:@"testValue" forKey:@"usedName" withMappingDictionary:mapping];
@@ -31,7 +41,21 @@
     STAssertEqualObjects([test valueForKey:@"actualName"], @"testValue", nil);
 }
 
+- (void) testTransformerMapping
+{
+    // Setup date (my birthdate)
+    NSDate * date = [NSDate dateWithTimeIntervalSince1970:372671100];
+
+    // Test
+    NSMutableDictionary * test = [NSMutableDictionary dictionary];
+    [test setValue:@"1981-10-23T07:45:00Z" forKey:@"usedDate" withMappingDictionary:mapping];
+    STAssertEqualObjects([test valueForKey:@"actualDate"], date, nil);
+}
+
 @end
+
+/****************************************************************************/
+#pragma mark -
 
 @interface NSManagedObject_KVCMapping_Tests : SenTestCase
 @end
@@ -65,34 +89,34 @@
                @"actualInt64", @"usedInt64",
                @"actualString", @"usedString",
                nil];
-    
+
     NSDate * date = [NSDate date];
     
     goodDataset = [NSDictionary dictionaryWithObjectsAndKeys:
-               [NSNumber numberWithBool:YES], @"usedBoolean",
-               [NSNumber numberWithShort:100], @"usedInt16",
-               [NSNumber numberWithInt:100], @"usedInt32",
-               [NSNumber numberWithLongLong:100], @"usedInt64",
-               [NSDecimalNumber numberWithInt:100], @"usedDecimal",
-               [NSNumber numberWithFloat:100], @"usedFloat",
-               [NSNumber numberWithDouble:100], @"usedDouble",
-               @"100", @"usedString",
-               [@"test" dataUsingEncoding:NSUTF8StringEncoding], @"usedData",
-               date, @"usedDate",
-               nil];
-
+                   [NSNumber numberWithBool:YES], @"usedBoolean",
+                   [NSNumber numberWithShort:100], @"usedInt16",
+                   [NSNumber numberWithInt:100], @"usedInt32",
+                   [NSNumber numberWithLongLong:100], @"usedInt64",
+                   [NSDecimalNumber numberWithInt:100], @"usedDecimal",
+                   [NSNumber numberWithFloat:100], @"usedFloat",
+                   [NSNumber numberWithDouble:100], @"usedDouble",
+                   @"100", @"usedString",
+                   [@"test" dataUsingEncoding:NSUTF8StringEncoding], @"usedData",
+                   date, @"usedDate",
+                   nil];
+    
     badDataSet = [NSDictionary dictionaryWithObjectsAndKeys:
-                @"YES", @"usedBoolean",
-                @"100", @"usedInt16",
-                @"100", @"usedInt32",
-                @"100", @"usedInt64",
-                @"100", @"usedDecimal",
-                @"100", @"usedFloat",
-                @"100", @"usedDouble",
-                [NSNumber numberWithInt:100], @"usedString",
-                [@"test" dataUsingEncoding:NSUTF8StringEncoding], @"usedData",
-                date, @"usedDate",
-               nil];
+                  @"YES", @"usedBoolean",
+                  @"100", @"usedInt16",
+                  @"100", @"usedInt32",
+                  @"100", @"usedInt64",
+                  @"100", @"usedDecimal",
+                  @"100", @"usedFloat",
+                  @"100", @"usedDouble",
+                  [NSNumber numberWithInt:100], @"usedString",
+                  [@"test" dataUsingEncoding:NSUTF8StringEncoding], @"usedData",
+                  date, @"usedDate",
+                  nil];
 }
 
 - (void) testBasic
@@ -103,7 +127,7 @@
     STAssertEqualObjects([test valueForKey:@"actualString"], @"testValue", nil);
 }
 
-- (void) testGoodDataset
+- (void) testSimpleDataset
 {
     NSManagedObject * test = [NSEntityDescription insertNewObjectForEntityForName:@"TestEntity" inManagedObjectContext:moc];
     [test setValuesForKeysWithDictionary:goodDataset withMappingDictionary:mapping];
@@ -115,7 +139,7 @@
     }
 }
 
-- (void) testBadDataset
+- (void) testAutomaticCoercionDataset
 {
     NSManagedObject * test = [NSEntityDescription insertNewObjectForEntityForName:@"TestEntity" inManagedObjectContext:moc];
     [test setValuesForKeysWithDictionary:badDataSet withMappingDictionary:mapping];
