e9e18728aebcc26876d94fdcec2c5c6f7b991260
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2012-07-15 09:33:50 +0200

    Add outlets to PrefsVC and links to web pages.

--- Files/PrefsVC.m
+++ Files/PrefsVC.m
@@ -8,11 +8,50 @@
 
 #import "PrefsVC.h"
 
-@interface PrefsVC ()
-
+@interface PrefsVC () <UITableViewDataSource, UITableViewDelegate>
+@property (weak) IBOutlet UISegmentedControl *radarZonesSizeSegmentedControl;
+@property (strong) IBOutletCollection(UITableViewCell) NSArray *cells;
 @end
 
 @implementation PrefsVC
 
+/****************************************************************************/
+#pragma mark TableView
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.cells count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return [self.cells objectAtIndex:indexPath.row];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+    if(indexPath.row==0)
+        [self openDesignPage];
+    else
+        [self openSourcePage];
+}
+
+/****************************************************************************/
+#pragma mark Actions
+
+- (IBAction)openDesignPage {
+    NSString * designURL = [NSString stringWithFormat:@"http://%@",[[[NSBundle mainBundle] infoDictionary] objectForKey:@"designURL"]];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:designURL]];    
+}
+
+- (IBAction)openSourcePage {
+    NSString * sourceURL = [NSString stringWithFormat:@"http://%@",[[[NSBundle mainBundle] infoDictionary] objectForKey:@"sourceURL"]];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:sourceURL]];
+}
+
+- (IBAction)donate {
+}
+- (IBAction)changeRadarZone {
+}
+- (IBAction)updateStationsList {
+}
 
 @end
