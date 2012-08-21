96f0b7942fb462930055e0bc9575be2f06a6f28b
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2012-08-21 16:20:22 +0200

    WIP add DataGrab tooling

---
+++ Files/BicycletteDataGrab.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BicycletteDataGrab
+//
+//  Created by Nicolas Bouilleaud on 21/08/12.
+//  Copyright (c) 2012 Nicolas Bouilleaud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
