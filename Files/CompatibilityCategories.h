1eafcad5c09cb86d3927483d3be3b4aeff84b22d
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2012-09-28 21:32:17 +0200

    Remove now useless subscripting category

--- Files/CompatibilityCategories.h
+++ Files/CompatibilityCategories.h
@@ -9,21 +9,6 @@
 /****************************************************************************/
 #pragma mark -
 
-// http://petersteinberger.com/blog/2012/using-subscripting-with-Xcode-4_4-and-iOS-4_3/
-
-// Add support for subscripting to the iOS 5 SDK.
-#if __IPHONE_OS_VERSION_MAX_ALLOWED < 60000
-@interface NSObject (PSPDFSubscriptingSupport)
-
-- (id)objectAtIndexedSubscript:(NSUInteger)idx;
-- (void)setObject:(id)obj atIndexedSubscript:(NSUInteger)idx;
-- (void)setObject:(id)obj forKeyedSubscript:(id <NSCopying>)key;
-- (id)objectForKeyedSubscript:(id)key;
-
-@end
-#endif
-
-
 // iOS 6 only method, when compiling wiht iOS 5 SDK
 @interface NSString (CompatibilityCategories)
 - (NSString *)capitalizedStringWithLocale:(NSLocale *)locale;
