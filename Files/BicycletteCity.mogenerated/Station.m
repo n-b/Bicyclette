b28e79930ed0a9accb2716c25c75a14a37584cca
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2013-03-21 17:54:24 +0100

    One more fix for Marchfeld / Nextbike

--- Files/BicycletteCity.mogenerated/Station.m
+++ Files/BicycletteCity.mogenerated/Station.m
@@ -95,7 +95,7 @@
                              NSValidationObjectErrorKey : self,
                                 NSValidationKeyErrorKey : @"location",
                               NSValidationValueErrorKey : self.location,
-                              NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Station %@", 0),self.name],
+                              NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Station %@ (%@)", 0), self.number, self.name],
                        NSLocalizedFailureReasonErrorKey : [NSString stringWithFormat:NSLocalizedString(@"Invalid coordinates (%f, %f)", 0),self.location.coordinate.latitude, self.location.coordinate.longitude],
                                  }];
         *error = [NSError errorFromOriginalError:*error error:limitsError];
